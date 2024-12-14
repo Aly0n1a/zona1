848675d0ee849e6d7aa4ddd4bb1228540763d731
Чмирёва Алёна <meloman.ytxd@gmail.com>
2024-12-14 18:09:06 +0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Zona from './components/Zona/Zona'
 import './App.css'
 import Ps5 from './components/Ps5/Ps5'
 import ZonaStart from './components/ZonaStart/ZonaStart'
+import Camp from './components/Camp/Camp'
 
 
 
@@ -11,6 +12,7 @@ const App = () => {
     <>
     <Zona text={"СТАНДАРТ"} color={"pink"}/>
     <ZonaStart text={"СТАНДАРТ+"} color={"blue"} />
+    <Camp text={"BOOT CAMP"} color={"red"}/>
  
     <Ps5 text={"Ps5"} color={"sin"}/>
    
